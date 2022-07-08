4ac0b0251fc2d664d146efc3d8446fd3c64285a5
Alefe Oliveira <alefe.oliveira@sinch.com>
2022-07-08 15:51:12 -0300

    adding logic to increment rates

--- lambda-functions/rate-movie/index.js
+++ lambda-functions/rate-movie/index.js
@@ -4,40 +4,69 @@ const localstack_host = 'http://aws-localstack:4566'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient({ endpoint: localstack_host });
 
+const RATED_MOVIE_TABLE = 'rated-movie-table'
 
 async function createItem(params){
   try {
     await dynamodb.put(params).promise();
   } catch (err) {
+    console.error(err)
     return err;
+  }
+}
+
+async function getItems() {
+  try {
+    const params = {
+      TableName: RATED_MOVIE_TABLE
+     };
+   
+     return await dynamodb.scan(params).promise();
+  }catch(err) {
+    console.error(err)
+    return err
   }
 }
 
 
 exports.handler = async (event, context, callback) => {
   try {
+   
+    var items = []
+    var itemGradeEvaluated = 0
+
+    await getItems().then(data => {
+      console.log(JSON.stringify(data) )
+      items = data.Items
+    }).catch(error => {
+    console.error(error)
+   })
+
+   items.forEach(item => {
+    itemGradeEvaluated = item.grade_evaluated
+   })
+
+
     event.Records.forEach(async record => {
       var message = JSON.parse(record.body);
-
-
       const params = {
-        TableName : 'rated-movie-table',
+        TableName : RATED_MOVIE_TABLE,
         Item: {
            id: message.id,
            title: message.title,
            overview: message.overview,
            release_date: message.release_date,
            poster_path: message.poster_path,
-           grade_evaluated:  message.grade_evaluated
+           grade_evaluated: (+itemGradeEvaluated) + (+message.grade_evaluated)
         }
       }
-      console.log(params)
       await createItem(params)
      
     })
 
     return { body: 'Successfully created item!' }
   } catch (err) {
+    console.error(err)
     return { error: err }
   }
 
